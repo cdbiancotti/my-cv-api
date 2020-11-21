287bad6ab3d98fa88701e33465418826872d000b
Cristian D Biancotti <cdbiancotti@gmail.com>
2020-11-21 11:17:09 -0300

    Api first version

---
+++ api.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.route('/')
+def index():
+    info = {
+        "mensaje": "Bienvenido a la API del curriculum vitae de Cristian Biancotti.",
+        "acciones": [
+            "GET /curriculum",
+            "POST /mensajes"
+        ]
+    }
+    return jsonify(info)
+
+
+@app.route('/curriculum', methods=['GET'])
+def cv():
+    url_imagen = request.host_url + "static/api_test_img.jpg"
+    cv = {
+        "nombre": "Cristian Daniel",
+        "apellido": "Biancotti",
+        "residencia": "Argentina",
+        "experiencia": [{
+            "posicion": "< describe tu posición>",
+            "empresa": "< nombre de tu empresa >",
+            "desde": "< cuándo empezaste a trabajar >",
+            "hasta": "< si ya no trabajas más, cuándo >",
+            "descripcion": "< detalles >"
+        }],
+        "educación": {
+            "nivel": "< nivel de tus estudios >",
+            "titulo": "< nombre de tu carrera >",
+            "institucion": "< dónde estudiaste >",
+            "facultad": "< más detalles >"
+        },
+        "intereses": ["python", "apis", "enseñar"],
+        "redes": {
+            "github": "https://github.com/cdbiancotti",
+            "twitter": "https://twitter.com/cdbiancotti",
+            "linkedin": "https://www.linkedin.com/in/cdbiancotti"
+        },
+        "foto": url_imagen
+    }
+    return jsonify(cv)
+
+
+@app.route('/mensajes', methods=['POST'])
+def contacto():
+    mensaje = request.get_data()
+    if not mensaje:
+        abort(400, description="Debe enviar su mensaje en el body del POST.")
+    print("MENSAJE DE CONTACTO: " + str(mensaje))
+    return "Gracias por su mensaje."
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run()
